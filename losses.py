f52d2cbd7c9d89565d696e314583adf5d07951ad
Umberto Griffo <umberto.griffo@gmail.com>
2019-01-29 09:25:16 +0100

    minor tweaks

--- losses.py
+++ losses.py
@@ -46,14 +46,23 @@ def categorical_focal_loss(gamma=2., alpha=.25):
     Softmax version of focal loss.
 
            m
-      FL = ∑  -alpha * (1 - p_o,c)**gamma * y_o,c * log(p_o,c)
+      FL = ∑  -alpha * (1 - p_o,c)^gamma * y_o,c * log(p_o,c)
           c=1
 
       where m = number of classes, c = class and o = observation
 
+    Parameters:
+      alpha -- the same as weighing factor in balanced cross entropy
+      gamma -- focusing parameter for modulating factor (1-p)
+
+    Default value:
+      gamma -- 2.0 as mentioned in the paper
+      alpha -- 0.25 as mentioned in the paper
+
     References:
-        https://arxiv.org/pdf/1708.02002.pdf
+        Official paper: https://arxiv.org/pdf/1708.02002.pdf
         https://www.tensorflow.org/api_docs/python/tf/keras/backend/categorical_crossentropy
+
     Usage:
      model.compile(loss=[categorical_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
     """
@@ -64,28 +73,30 @@ def categorical_focal_loss(gamma=2., alpha=.25):
         :return: Output tensor.
         """
 
-        # scale predictions so that the class probas of each sample sum to 1
+        # Scale predictions so that the class probas of each sample sum to 1
         y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
 
+        # Clip the prediction value to prevent NaN's and Inf's
         epsilon = K.epsilon()
-        # clip to prevent NaN's and Inf's
         y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
 
-        # Cross_entropy
-        cross_entropy = y_true * K.log(y_pred)
-        # Focal Loss
+        # Calculate Cross Entropy
+        cross_entropy = -y_true * K.log(y_pred)
+
+        # Calculate Focal Loss
         loss = alpha * K.pow(1 - y_pred, gamma) * cross_entropy
 
-        return -K.sum(loss, -1)
+        # Sum the losses in mini_batch
+        return K.sum(loss, axis=1)
 
     return categorical_focal_loss_fixed
 
 
 if __name__ == '__main__':
+
     # Test serialization of nested functions
     bin_inner = dill.loads(dill.dumps(binary_focal_loss(gamma=2., alpha=.25)))
     print(bin_inner)
 
     cat_inner = dill.loads(dill.dumps(categorical_focal_loss(gamma=2., alpha=.25)))
     print(cat_inner)
-
